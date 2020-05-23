c9af81b1e54f594c9f4a0a3b1cb9ff1755c5d857
cahlchang <kahlua.dane@gmail.com>
2020-05-23 19:16:47 +0900

    Add dev code

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,55 @@
 <template>
 <div id="box">
     <div id="app">
-        <h2>Cthulhu TRPG Charasheet</h2>
-        <h3>基本情報</h3>
-        <div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>test</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+        <h2>Cthulhu TRPG Charasheet ver 6</h2>
+
+        <div onclick="obj=document.getElementById('open_kihon').style; obj.display=(obj.display=='none')?'block':'none';">
+            <a style="cursor:pointer;"><h3>基本情報</h3></a>
         </div>
-        <h3>能力値</h3>
-        <div>
+        <div id="open_kihon">
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>test</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div onclick="obj=document.getElementById('open_param').style; obj.display=(obj.display=='none')?'block':'none';">
+            <a style="cursor:pointer;"><h3>能力値</h3></a>
+        </div>
+
+        <div id="open_param">
             <table>
                 <thead>
                     <tr>
                         <template v-for='(param, index) in param_items'>
-                            <th :key="index">{{ param }}</th>
+                            <th :key="index">{{ param.name }}</th>
                         </template>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
                         <template v-for='(param, index) in param_items'>
-                            <td :key=param+index>
+                            <td :key=param+index v-if='param.type==0'>能力値</td>
+                            <td :key=param+index v-if='param.type==1 || param.type==2 || param.type==3'>
                                 <div class="cp_ipselect cp_sl04">
                                     <select v-model="selected">
-                                        <option v-for='(value,index_value) in param_value_list' :key='index_value'>
+                                        <option v-for='(value,index_value) in param_value_list_type_1' :key='index_value' v-if='param.type==1'>
+                                            {{ value }}
+                                        </option>
+                                        <option v-for='(value,index_value) in param_value_list_type_2' :key='index_value' v-if='param.type==2'>
+                                            {{ value }}
+                                        </option>
+                                        <option v-for='(value,index_value) in param_value_list_type_3' :key='index_value' v-if='param.type==3'>
                                             {{ value }}
                                         </option>
                                     </select>
@@ -44,22 +59,36 @@
                     </tr>
                     <tr>
                         <template v-for='(param, index) in param_items'>
-                            <td :key=param+index>
+                            <td :key=param+index  v-if='param.type==0'>増加分
+                            </td>
+                            <td :key=param+index  v-if='param.type==1 || param.type==2 || param.type==3 || param.type==4'>
+                                <input type="text" name="name" maxlength="2" class="input_box_param"/>
                             </td>
                         </template>
                         </tr>
                         <tr>
                             <template v-for='(param, index) in param_items'>
-                                <td :key=param+index>
+                                <td :key=param+index  v-if='param.type==0'>一時的
+                                </td>
+                                <td :key=param+index  v-if='param.type==1 || param.type==2 || param.type==3 || param.type==4'>
+                                    <input type="text" name="name" maxlength="2" class="input_box_param"/>
+                                </td>
+                            </template>
+                        </tr>
+                        <tr>
+                            <template v-for='(param, index) in param_items'>
+                                <td :key=param+index  v-if='param.type==0'>現在値
+                                </td>
+                                <td :key=param+index  v-if='param.type==1 || param.type==2 || param.type==3'>
                                 </td>
                             </template>
                         </tr>
                 </tbody>
             </table>
-            <h3>戦闘技能</h3>
-            <h3>探索技能</h3>
-            <h3>技能</h3>
         </div>
+        <h3>戦闘技能</h3>
+        <h3>探索技能</h3>
+        <h3>技能</h3>
     </div>
 </div>
 
@@ -76,24 +105,30 @@ export default {
       authButtonText: null,
       authFunction: function () {},
       param_items: [
-        '',
-        'STR',
-        'CON',
-        'POW',
-        'DEX',
-        'APP',
-        'SIZ',
-        'INT',
-        'EDU',
-        'HP',
-        'MP',
-        'SAN',
-        'IDE',
-        '幸運',
-        '知識'
+        {'name': '　能力値　', 'type': 0},
+        {'name': 'STR', 'type': 1},
+        {'name': 'CON', 'type': 1},
+        {'name': 'POW', 'type': 1},
+        {'name': 'DEX', 'type': 1},
+        {'name': 'APP', 'type': 1},
+        {'name': 'SIZ', 'type': 1},
+        {'name': 'INT', 'type': 2},
+        {'name': 'EDU', 'type': 3},
+        {'name': 'HP', 'type': 4},
+        {'name': 'MP', 'type': 4},
+        {'name': 'SAN', 'type': 4},
+        {'name': 'IDE', 'type': 4},
+        {'name': '幸運', 'type': 4},
+        {'name': '知識', 'type': 4}
       ],
-      param_value_list: [
-        '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18'
+      param_value_list_type_1: [
+        '', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18'
+      ],
+      param_value_list_type_2: [
+        '', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18'
+      ],
+      param_value_list_type_3: [
+        '', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21'
       ]
     }
   }
@@ -109,6 +144,9 @@ body {
 }
 
 #app {
+  width: 60%;
+  margin-left: auto;
+  margin-right: auto;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -118,12 +156,12 @@ body {
 
 table{
   border-collapse: separate;
-  border-spacing: 5px;
+  border-spacing: 3px;
   margin: 0 auto;
 }
 td,th{
-  padding: 10px;
   text-align: center;
+  white-space: nowrap;
 }
 th:first-of-type,
 td:first-of-type{
@@ -146,6 +184,8 @@ td{
  text-overflow: ellipsis;
  border: none;
  outline: none;
+ text-align: center;
+ font-size: 18px;
  background: transparent;
  background-image: none;
  box-shadow: none;
@@ -153,7 +193,7 @@ td{
  appearance: none;
 }
 .cp_ipselect select::-ms-expand {
-    display: none;
+ display: none;
 }
 .cp_ipselect.cp_sl04 {
  position: relative;
@@ -178,5 +218,21 @@ td{
 .cp_ipselect.cp_sl04 select {
  padding: 8px 28px 8px 8px;
  color: #000;
+}
+.input_box_param {
+  width:100%;
+  height:10px;
+  border:1px;
+  background-color: #746E62;
+  padding:10px 0px;
+  font-size: 22px;
+  color: #fff;
+  text-align: center;
+}
+.input_box_param input[type="text"] {
+  width:100%;
+  -moz-box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
 }
 </style>
