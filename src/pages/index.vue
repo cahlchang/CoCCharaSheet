0fea10cb1909ffa01d866938974b370d567f79e7
cahlchang <kahlua.dane@gmail.com>
2020-05-30 12:30:46 +0900

    ベタに移植

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,92 +1,23 @@
 <template>
-    <v-layout
-        column
-        justify-center
-        align-center
-    >
-        <v-flex
-            xs12
-            sm8
-            md6
-        >
-            <div class="text-center">
-                <logo />
-                <vuetify-logo />
-            </div>
-            <v-card>
-                <v-card-title class="headline">
-                    Welcome CoC Charaseet
-                </v-card-title>
-                <v-card-text>
-                    <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-                    <p>
-                        For more information on Vuetify, check out the <a
-                                                                           href="https://vuetifyjs.com"
-                                                                           target="_blank"
-                                                                       >
-              documentation
-                        </a>.
-                    </p>
-                    <p>
-                        If you have questions, please join the official <a
-                                                                            href="https://chat.vuetifyjs.com/"
-                                                                            target="_blank"
-                                                                            title="chat"
-                                                                        >
-                        discord
-                        </a>.
-                    </p>
-                    <p>
-                        Find a bug? Report it on the github <a
-                                                                href="https://github.com/vuetifyjs/vuetify/issues"
-                                                                target="_blank"
-                                                                title="contribute"
-                                                            >
-                        issue board
-                        </a>.
-                    </p>
-                    <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-                    <div class="text-xs-right">
-                        <em><small>&mdash; John Leider</small></em>
-                    </div>
-                    <hr class="my-3">
-                    <a
-                        href="https://nuxtjs.org/"
-                        target="_blank"
-                    >
-                        Nuxt Documentation
-                    </a>
-                    <br>
-                    <a
-                        href="https://github.com/nuxt/nuxt.js"
-                        target="_blank"
-                    >
-                        Nuxt GitHub
-                    </a>
-                </v-card-text>
-                <v-card-actions>
-                    <v-spacer />
-                    <v-btn
-                        color="primary"
-                        nuxt
-                        to="/inspire"
-                    >
-                        Continue
-                    </v-btn>
-                </v-card-actions>
-            </v-card>
-        </v-flex>
-    </v-layout>
+<div id="box">
+    <div id="app">
+        <h2>Cthulhu TRPG Charasheet ver 6</h2>
+
+        <info-table-box></info-table-box>
+        <param-table-box></param-table-box>
+
+    </div>
+</div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+import InfoTableBox from '@/components/InfoTableBox.vue'
+import ParamTableBox from '@/components/ParamTableBox.vue'
 
 export default {
   components: {
-    Logo,
-    VuetifyLogo
+    InfoTableBox,
+    ParamTableBox
   }
 }
 </script>
